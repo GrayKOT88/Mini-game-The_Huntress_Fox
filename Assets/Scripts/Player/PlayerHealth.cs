eeb724ce1ce6717914650f7ed5fb06e95da448b1
GrayKOT88 <skotkin@yandex.ru>
2025-10-13 16:09:39 +0300

    13.10.25

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,6 +7,13 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private GameConfig _gameConfig;
     [SerializeField] private Slider healthSlider;
     [SerializeField] private GameObject damageOverlay;
+
+    [SerializeField] private Image healthFillImage;
+    private Color fullHealthColor = Color.green;
+    private Color mediumHealthColor = Color.yellow;
+    private Color lowHealthColor = Color.red;
+    private Color targetColor;
+
     public event System.Action OnDeath;
 
     public int CurrentHealth { get; private set; }    
@@ -17,6 +24,8 @@ public class PlayerHealth : MonoBehaviour
         if(CurrentHealth <= 0) { CurrentHealth = _gameConfig.PlayerMaxHealth; }
         healthSlider.maxValue = _gameConfig.PlayerMaxHealth;
         healthSlider.value = CurrentHealth;
+
+        UpdateHealthColor();
     }
 
     public void TakeDamage(int damage)
@@ -25,6 +34,7 @@ public class PlayerHealth : MonoBehaviour
         CurrentHealth = Mathf.Clamp(CurrentHealth - damage, 0, _gameConfig.PlayerMaxHealth);
         healthSlider.value = CurrentHealth;
         PlayerPrefs.SetInt("Health", CurrentHealth);
+        UpdateHealthColor();
 
         if (damage > 0)
         {
@@ -42,6 +52,29 @@ public class PlayerHealth : MonoBehaviour
         CurrentHealth = Mathf.Clamp(CurrentHealth + amount, 0, _gameConfig.PlayerMaxHealth);
         healthSlider.value = CurrentHealth;
         PlayerPrefs.SetInt("Health", CurrentHealth);
+        UpdateHealthColor();
+    }
+
+    private void UpdateHealthColor()
+    {
+        float healthPercentage = (float)CurrentHealth / _gameConfig.PlayerMaxHealth;
+
+        if (healthPercentage > 0.5f)    // Зеленый ? Желтый (100% - 50%)
+        {
+            float t = (healthPercentage - 0.5f) / 0.5f;
+            targetColor = Color.Lerp(mediumHealthColor, fullHealthColor, t);
+        }
+
+        else   // Желтый ? Красный (50% - 0%)
+        {            
+            float t = healthPercentage / 0.5f;
+            targetColor = Color.Lerp(lowHealthColor, mediumHealthColor, t);
+        }
+
+        if (healthFillImage != null)
+        {
+            healthFillImage.color = targetColor;
+        }
     }
 
     private IEnumerator ShowDamageEffect()
